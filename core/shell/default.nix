403a318d5fc4c36ebd86c72bbac3d342f8db4b8f
Adithya Ajith <adithya@satyaloka.org>
2024-08-22 21:41:03 +0000

    Theme starship prompt

--- core/shell/default.nix
+++ core/shell/default.nix
@@ -1,7 +1,7 @@
-{
-  ...
-}
-:
-{
+{...}
+: {
   programs.fish.enable = true;
+  programs.fish.shellInit = ''
+    source (/home/adi/.nix-profile/bin/starship init fish --print-full-init | psub)
+  '';
 }
--- home/gui/hyprland/general.nix
+++ home/gui/hyprland/general.nix
@@ -1,9 +1,9 @@
 {...}
 : {
   wayland.windowManager.hyprland.settings.general = {
-    gaps_in = 2;
+    gaps_in = 5;
     gaps_out = 10;
-    border_size = 2;
+    border_size = 4;
     layout = "dwindle";
     resize_on_border = true;
     "col.active_border" = "$outline $outline1";
